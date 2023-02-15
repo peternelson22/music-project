734c87fe8fb324148f72994af7f67513b3f0608e
jaeabiola-Git <jideabiola@yahoo.com>
2023-02-15 14:07:12 +0000

    change to title

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,6 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-# python manage.py loaddata adminuser
+python manage.py loaddata adminuser
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi:application
